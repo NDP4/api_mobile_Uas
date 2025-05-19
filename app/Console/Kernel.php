d3c4047dd3bf17c14b5532ab234e3da8a0f2d100
NDP4 <nurdwipriyambodo@proton.me>
2025-05-19 14:03:26 +0700

    penambahan komen storage link

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\StorageLinkCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\RouteListCommand::class
+        Commands\RouteListCommand::class,
+        StorageLinkCommand::class
     ];
 
     /**
